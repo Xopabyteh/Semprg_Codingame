bbd8fa970c3b1f7b255876c75966fb25c53b6059
Xopabyteh <70832276+Xopabyteh@users.noreply.github.com>
2023-09-19 19:25:33 +0200

    done

--- Semprg_Codingame/DeathFirstSearchEpisode1.cs
+++ Semprg_Codingame/DeathFirstSearchEpisode1.cs
@@ -1,86 +1,95 @@
-﻿using System;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
+﻿    using System;
+    using System.Linq;
+    using System.IO;
+    using System.Text;
+    using System.Collections;
+    using System.Collections.Generic;
 
-class Player
-{
-    static void Main(string[] args)
+    class Player
     {
-        var inputs = Console.ReadLine().Split(' ');
-        var totalNodes = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
-        var totalLinks = int.Parse(inputs[1]); // the number of links
-        var totalExits = int.Parse(inputs[2]); // the number of exit gateways
-        
-        //var links = new List<Link>(totalLinks);
-
-        //Key: node, value: nodes that are connected to the key node
-        var links = new Dictionary<int, List<int>>();
-        var exits = new int[totalExits];
-
-        for (int i = 0; i < totalLinks; i++)
+        static void Main(string[] args)
         {
-            inputs = Console.ReadLine().Split(' ');
-            var n1 = int.Parse(inputs[0]); // N1 and N2 defines a link between these nodes
-            var n2 = int.Parse(inputs[1]);
-            //links[i] = new Link(n1, n2);
+            var inputs = Console.ReadLine().Split(' ');
+            var totalNodes = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
+            var totalLinks = int.Parse(inputs[1]); // the number of links
+            var totalExits = int.Parse(inputs[2]); // the number of exit gateways
 
-            if (!links.ContainsKey(n1))
+            var links = new List<Link>(totalLinks);
+
+            var exits = new int[totalExits];
+
+            for (int i = 0; i < totalLinks; i++)
             {
-                links[n1] = new List<int>();
+                inputs = Console.ReadLine().Split(' ');
+                var n1 = int.Parse(inputs[0]); // N1 and N2 defines a link between these nodes
+                var n2 = int.Parse(inputs[1]);
+                links.Add(new Link(n1, n2));
             }
-            links[n1].Add(n2);
-        }
-        for (int i = 0; i < totalExits; i++)
-        {
-            var exitNode = int.Parse(Console.ReadLine()); // the index of a gateway node
-            exits[i] = exitNode;
-        }
-
-        while (true)
-        {
-            var virusNode = int.Parse(Console.ReadLine()); // The index of the node on which the Bobnet agent is positioned this turn
-
-            //Console.Error.WriteLine($"links:");
-            //foreach (var link in links)
-            //{
-            //    Console.Error.WriteLine($"{link.Key}: {string.Join(", ", link.Value)}");
-            //}
-
-            //break;
-            //Find the nearest link that goes from the virus to the nearest exit
-            
-            var bfsNodes = new Queue<int>(links.Count);
-            var visitedNodes = new List<int>(bfsNodes.Count);
-
-            //Add all nodes going from the virus
-            foreach (var connectedNode in links[virusNode])
+            for (int i = 0; i < totalExits; i++)
             {
-                bfsNodes.Enqueue(connectedNode);
+                var exitNode = int.Parse(Console.ReadLine()); // the index of a gateway node
+                exits[i] = exitNode;
             }
 
-            //Search until we reach an exit from the virus, 
-            while (visitedNodes.Count != bfsNodes.Count)
+            while (true)
             {
-                var searchNode = bfsNodes.Dequeue();
-                visitedNodes.Add(searchNode);
+                var virusNode = int.Parse(Console.ReadLine()); // The index of the node on which the Bobnet agent is positioned this turn
 
-                foreach (var connectedNode in links[searchNode])
+                var bfsNodes = new Queue<int>(links.Count);
+                var visitedNodes = new List<int>(bfsNodes.Count);
+
+                //Start with virus node and go until we find an exit
+                bfsNodes.Enqueue(virusNode);
+
+                while (visitedNodes.Count < bfsNodes.Count)
                 {
-                    bfsNodes.Enqueue(connectedNode);
-                    
-                    //If we've reached an exit
-                    if (exits.Contains(searchNode))
+                    //While there are nodes to visit
+
+                    var searchNode = bfsNodes.Dequeue();
+                    Console.Error.WriteLine($"Searching {searchNode}");
+
+                    visitedNodes.Add(searchNode);
+
+                    var connectedNodes = GetNodesConnectedTo(searchNode, links);
+                    foreach (var connectedNode in connectedNodes)
                     {
-                        Console.WriteLine($"{searchNode} {connectedNode}");
-                        return;
+                        //If node is exit, we're done
+                        if (exits.Contains(connectedNode))
+                        { 
+                            Console.WriteLine($"{searchNode} {connectedNode}");
+                            goto nextIteration;
+                        }
+
+                        //Otherwise, add to bfs for further searching
+                        if (!visitedNodes.Contains(connectedNode))
+                        {
+                            bfsNodes.Enqueue(connectedNode);
+                        }
                     }
                 }
-            }
 
-            Console.Error.WriteLine("No exit found");
+                Console.Error.WriteLine("No exit found");
+
+                nextIteration: ;
+            }
+        }
+
+        private static int[] GetNodesConnectedTo(int node, IEnumerable<Link> links)
+        {
+            return links
+                .Where(x => x.Node1 == node || x.Node2 == node)
+                .Select(x => x.Node1 == node ? x.Node2 : x.Node1).ToArray();
+        }
+
+        private readonly struct Link
+        {
+            public readonly int Node1;
+            public readonly int Node2;
+
+            public Link(int node1, int node2)
+            {
+                Node1 = node1;
+                Node2 = node2;
+            }
         }
     }
-}
